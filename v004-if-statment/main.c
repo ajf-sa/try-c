dabf7b7f190564543922a68d3a815fef9f6f9b70
alfuhigi <abajall@gmail.com>
2022-05-25 22:11:41 +0300

    if statment

---
+++ v004-if-statment/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 21;
+    if (age >= 18)
+    {
+        printf("You Can Watch \n");
+    }
+    else
+    {
+        printf("You Can't Watch \n");
+    }
+}
