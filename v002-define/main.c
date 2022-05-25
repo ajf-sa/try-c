a152243cbbf832a4a2a59ea304f4655d94b53028
alfuhigi <abajall@gmail.com>
2022-05-25 22:11:14 +0300

    define

---
+++ v002-define/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define AGE 20
+#define HEIGHT 1.75
+
+int main()
+{
+    // Print the age and height of the person
+    printf("My age is %d and I'm %f\n", AGE, HEIGHT);
+    return 0;
+}
